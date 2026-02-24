86135a990e68da85900dfe1e25e058dd3ea2b7bd
Thilina Nirmal <thilina@evotech.lk>
2026-02-25 01:33:15 +0530

    error fixing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // මේ පේළිය අලුතින් දාන්න
+
 export default defineConfig({
-  plugins: [react()],
-  build: { cssMinify: 'esbuild' }
+  plugins: [
+    react(),
+    tailwindcss(), // මේකත් ඇතුළට දාන්න
+  ],
+  build: {
+    cssMinify: 'esbuild'
+  }
 })
